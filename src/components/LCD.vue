ea5bfad21dba9da02b7e29104ef32602d6fd807c
zhengyi <88762262+hauzhengyi@users.noreply.github.com>
2025-05-29 22:41:28 +0800

    [SETUP] Basic LCD grid testing

---
+++ src/components/LCD.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="background"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="sass">
+.background
+  width: 500px
+  height: 500px
+  background-image: linear-gradient(to bottom, transparent 15px, black 15px), linear-gradient(to right, #ff0000 5px, #00ff00 5px, #00ff00 10px, #0000ff 10px, #0000ff 15px, black 15px)
+  background-size: 20px 20px
+  background-position: 0 0
+</style>
